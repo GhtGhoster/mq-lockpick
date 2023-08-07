44f349498493b34705f8fae92e349f64205e7e76
GhtGhoster <32255934+GhtGhoster@users.noreply.github.com>
2023-08-08 01:48:47 +0200

    Lay out basic pin tumbler structure

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,6 @@
+#![allow(unused_parens)]
 
+use egui::Slider;
 use macroquad::prelude::*;
 
 #[macroquad::main("mq-lockpick")]
@@ -13,39 +15,38 @@ async fn main() {
     //  over the top - rendering vector art
     //  unrelated - add self-removal to the rename.ps1 script
 
-    let pixel_side_ratios = vec![
-        (0.0, 0.0),
-        (50.0, 30.0),
-        (100.0, 40.0),
-        (200.0, 57.0),
-        (300.0, 64.0),
-        (400.0, 70.0),
-        (500.0, 90.0),
-    ];
+    // fn draw_better_circle(x: f32, y: f32, radius: f32, color: Color) {
+    //     let sides: u8 = (std::f32::consts::PI / (radius.powf(0.4)/radius).asin()) as u8;
+    //     draw_poly(x, y, sides, radius, 0.0, color);
+    // }
 
-    let mut show_circle: bool = false;
-    let mut radius_power: f32 = 0.4;
-    let mut radius: f32 = screen_height().min(screen_width())/2.0 - 20.0;
-    let mut sides: u8 = (std::f32::consts::PI / (radius.powf(radius_power)/radius).asin()) as u8;
-    let mut sides_min: u8 = 10;
+    let mut pin_amount: u8 = 6;
+    let mut bitting: Vec<u8> = generate_bitting(pin_amount);
 
     loop {
         // ui
         egui_macroquad::ui(|egui_ctx| {
             egui::Window::new("Controls")
                 .show(egui_ctx, |ui| {
-                    ui.spacing_mut().slider_width = screen_width() - 100.0;
-                    ui.checkbox(&mut show_circle, "Show default circle");
-                    if ui.add(egui::Slider::new(&mut radius, 0.0..=screen_width().min(screen_height())/2.0)).changed() {
-                        sides = (std::f32::consts::PI / (radius.powf(radius_power)/radius).asin()) as u8;
+                    ui.horizontal(|ui| {
+                        ui.label("Pin amount:");
+                        if ui.add(Slider::new(&mut pin_amount, 1u8..=10u8)).changed() {
+                            bitting = generate_bitting(pin_amount);
+                        };
+                    });
+                    ui.horizontal(|ui| {
+                        ui.label("Bitting:");
+                        let string = bitting
+                            .as_slice()
+                            .into_iter()
+                            .map(|number| number.to_string())
+                            .collect::<Vec<String>>()
+                            .join(", ");
+                        ui.code(format!("[{string}]"));
+                    });
+                    if ui.button("Reroll bitting").clicked() {
+                        bitting = generate_bitting(pin_amount);
                     }
-                    if ui.add(egui::Slider::new(&mut radius_power, 0.1..=1.0)).changed() {
-                        sides = (std::f32::consts::PI / (radius.powf(radius_power)/radius).asin()) as u8;
-                    }
-                    ui.add(egui::Slider::new(&mut sides, u8::MIN..=u8::MAX));
-                    if ui.add(egui::Slider::new(&mut sides_min, 0..=20)).changed() {
-                        sides = (std::f32::consts::PI / (radius.powf(radius_power)/radius).asin()) as u8;
-                    };
                 }
             );
         });
@@ -53,36 +54,71 @@ async fn main() {
         // rendering
         clear_background(BLACK);
 
-        // graph
-        let factor_x = screen_width() / pixel_side_ratios[pixel_side_ratios.len()-1].0;
-        let factor_y = screen_height() / pixel_side_ratios[pixel_side_ratios.len()-1].1;
-        for i in 0..pixel_side_ratios.len()-1 {
-            let x = pixel_side_ratios[i].0 * factor_x;
-            let y = screen_height() - (pixel_side_ratios[i].1 * factor_y);
-            draw_text(
-                format!("{}, {}", pixel_side_ratios[i].0, pixel_side_ratios[i].1).as_str(),
-                x,
-                y,
-                20.0,
-                RED,
-            );
-            draw_circle(x, y, 5.0, RED);
-            draw_line(
-                x,
-                y,
-                pixel_side_ratios[i+1].0 * factor_x,
-                screen_height() - (pixel_side_ratios[i+1].1 * factor_y),
-                1.0,
-                RED,
-            );
-        }
+        // values useful for rendering
+        let pin_width = screen_width() / 32f32.max(((pin_amount as f32 * 2.0) + 2.0) * 2.0);
+        let pin_height = screen_height()/6.0;
 
-        // cirlce
-        if radius > 0.0 {
-            draw_poly(screen_width()/2.0, screen_height()/2.0, sides.max(sides_min), radius, 0.0, Color::from_rgba(255, 255, 0, 127));
-            if show_circle {
-                draw_circle(screen_width()/2.0, screen_height()/2.0, radius, Color::from_rgba(255, 0, 255, 127));
-            }
+        let plug_width = ((pin_amount as f32 * 2.0) + 1.0) * pin_width;
+        let plug_height = pin_height * 2.0;
+
+        let body_width = ((pin_amount as f32 * 2.0) + 2.0) * pin_width;
+        let body_height = ((pin_height*2.0) + pin_width) * 2.0;
+
+        // body
+        draw_rectangle(
+            (screen_width()/2.0) - body_width,
+            (screen_height()/2.0) - body_height / 2.0,
+            body_width,
+            body_height,
+            WHITE,
+        );
+
+        // plug cavity
+        draw_rectangle(
+            (screen_width()/2.0) - plug_width - (pin_width/8.0),
+            (screen_height()/2.0) - (pin_width/8.0),
+            plug_width + (pin_width/8.0),
+            plug_height + (pin_width/4.0),
+            DARKGRAY,
+        );
+
+        // plug
+        draw_rectangle(
+            (screen_width()/2.0) - plug_width,
+            (screen_height()/2.0),
+            plug_width,
+            plug_height,
+            WHITE,
+        );
+
+        // keyway
+        draw_rectangle(
+            (screen_width()/2.0) - pin_amount as f32 * 2.0 * pin_width,
+            (screen_height()/2.0) + pin_height,
+            pin_amount as f32 * 2.0 * pin_width,
+            pin_height,
+            DARKGRAY,
+        );
+
+        // pins
+        for i in 1..=pin_amount {
+            // plug cavity
+            draw_rectangle(
+                (screen_width()/2.0) - (i as f32 * 2.0 * pin_width),
+                (screen_height()/2.0),
+                pin_width,
+                pin_height,
+                DARKGRAY,
+            );
+
+            // body cavity
+            draw_rectangle(
+                (screen_width()/2.0) - (i as f32 * 2.0 * pin_width),
+                (screen_height()/2.0) - (pin_height*1.5),
+                pin_width,
+                pin_height + pin_height*1.5,
+                DARKGRAY,
+            );
         }
 
         egui_macroquad::draw();
@@ -90,4 +126,14 @@ async fn main() {
         // next frame
         next_frame().await
     }
+}
+
+fn generate_bitting(pin_amount: u8) -> Vec<u8> {
+    let mut bitting = Vec::with_capacity(pin_amount as usize);
+    for _ in 0..pin_amount {
+        bitting.push(
+            rand::gen_range(1u8, 10u8)
+        );
+    }
+    bitting
 }
